f7759a07001043d37d8be311d80b0212bcfe74cc
amilorea <mr.equal1996@gmail.com>
2021-02-15 11:02:08 +0700

    feat: link marker ring

--- src/component/link-marker/index.tsx
+++ src/component/link-marker/index.tsx
@@ -1,3 +1,6 @@
+import { CaretUpOutlined } from '@ant-design/icons';
+import { Tooltip } from 'antd';
+import React, { useState } from 'react';
 import './link-marker.scss';
 
 export type LinkMarker = {
@@ -14,10 +17,46 @@ export const LinkMarker = ({
 };
 
 export type LinkMarkChooser = {
-
+    defaultValue?: string[],
+    onChange?: (value: string[]) => void,
 };
 export const LinkMarkChooser = ({
-
+    defaultValue = [],
+    onChange = () => {},
 }: LinkMarkChooser) => {
+    const [choosenArrow, setChoosenArrow] = useState(defaultValue);
+    const rotateMap = [-45, 0, 45, -90, 0, 90, -135, 180, 135];
 
+    return <div className="link-marker-chooser">
+        {[...Array(9)].map((entry, index) => {
+            const normalizedIndex = `${index + 1}`;
+
+            if (normalizedIndex === '5') return <Tooltip key="5" overlay="Reset">
+                <div className="link-marker-reset" onClick={() => {
+                    onChange([]);
+                    setChoosenArrow([]);
+                }}>R</div>
+            </Tooltip>;
+
+            return <div key={normalizedIndex}
+                className={`link-marker-button marker-${index} ${choosenArrow.includes(normalizedIndex) ? 'marker-checked' : ''}`}
+                onClick={() => {
+                    onChange([]);
+                    setChoosenArrow(cur => {
+                        let newMap = [...cur];
+
+                        if (newMap.includes(normalizedIndex)) newMap = newMap.filter(entry => entry !== normalizedIndex);
+                        else newMap.push(normalizedIndex);
+
+                        onChange(newMap);
+                        return newMap;
+                    });
+                }}
+            >
+                <div className="link-marker-icon" style={{ transform: `rotate(${rotateMap[index]}deg)` }}>
+                    <CaretUpOutlined />
+                </div>
+            </div>;
+        })}
+    </div>;
 };
